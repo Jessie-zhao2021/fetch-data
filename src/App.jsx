116f71c2b946c02c8240eda40a8400b579b2c6c6
lovekwe <jessiezhao1816@gmail.com>
2023-03-23 14:41:04 +1100

    add effect function

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
 
   const [starWarsData, setStarWarsData] = useState({})
-    
+  const [count, setCount] = useState(1)  
   // console.log("Component rendered")
-  
-  fetch("https://swapi.dev/api/people/1")
-      .then(res => res.json())
-      .then(data => setStarWarsData(data))
+  useEffect(function() {
+    fetch(`https://swapi.dev/api/people/${count}`)
+    .then(res => res.json())
+    .then(data => setStarWarsData(data))
+  }, [count])
+
   
   return (
       <div>
+          <h1>The count is {count}</h1>
+          <button onClick={()=>setCount(prevCount=>prevCount+1)}>Go next character</button>
           <pre>{JSON.stringify(starWarsData, null, 2)}</pre>
       </div>
   )
